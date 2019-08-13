87262caf03262713193bbdd029083c2f5b83aac3
mosokolov <mosokolov@phoenixit.ru>
2019-08-13 14:41:57 +0300

    SCD-4 - Проработка способа взаимодейтсивя с Keycloak через протокол авторизации OAUTH2

--- src/main/java/ru/digital/league/x5/sign/bindings/config/oauth2/OAuth2WebSecurityConfigurerAdapter.java
+++ src/main/java/ru/digital/league/x5/sign/bindings/config/oauth2/OAuth2WebSecurityConfigurerAdapter.java
@@ -1,6 +1,6 @@
 package ru.digital.league.x5.sign.bindings.config.oauth2;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,14 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@ConditionalOnMissingBean(SecurityConfig.class)
 public class OAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Value("${security-enabled}")
+    private boolean securityEnabled;
 
     private List<String> ignoredURLS = new ArrayList<>();
 
     @Override
     public void configure(WebSecurity web) {
-        ignoredURLS.add("/**");
+        if (!securityEnabled) {
+            ignoredURLS.add("/**");
+        }
+        ignoredURLS.add("/actuator/**");
         web.ignoring().antMatchers(ignoredURLS.toArray(new String[0]));
     }
 }
