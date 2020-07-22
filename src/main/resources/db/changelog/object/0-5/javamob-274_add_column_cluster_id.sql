d4dbd2a7c0cf7c1afa04c1822c04634b3d097699
St.Yakovlev <St.Yakovlev@x5.ru>
2020-07-22 17:27:12 +0300

    javamob-274 * добавлен комментарий к полю stores.cluster_id

--- src/main/resources/db/changelog/object/0-5/javamob-274_add_column_cluster_id.sql
+++ src/main/resources/db/changelog/object/0-5/javamob-274_add_column_cluster_id.sql
@@ -2,3 +2,4 @@
 
 --changeSet St.Yakovlev:javamob-274 logicalFilePath:db/changelog/object/0-5/javamob-274_add_column_cluster_id.sql
 ALTER TABLE bindings.stores ADD COLUMN IF NOT EXISTS cluster_id varchar(20);
+COMMENT ON COLUMN bindings.stores.cluster_id IS 'Идентификатор кластера';
