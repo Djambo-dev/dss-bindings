34bac22873a7d7789014bf91562f4ce829df49b0
A.Lambert <Artem.Lambert@x5.ru>
2021-05-13 15:21:03 +0300

    JAVAMOB-669 * Добавление сотрудников сети магазинов "Чижик", правил отображения закрытых магазинов

---
+++ src/main/resources/db/changelog/object/0-8/employee_chizhik.sql
@@ -0,0 +1,50 @@
+--liquibase formatted sql
+
+--changeSet A.Lambert:employee_chizhik.sql logicalFilePath:db/changelog/object/0-8/employee_chizhik.sql
+
+--  cfo_id = E1040874
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040874','1663608', 56000001,'Управляющий магазином', 'KOMAROV.NIKOLAY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040874','1661372', 56000002,'Территориальный менеджер', 'BELOZEROV.LEV', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040874','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+
+--  cfo_id = E1040873
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040873','1678992', 56000001,'Управляющий магазином', 'MIKLYAEV.IGOR', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040873','1639616', 56000002,'Территориальный менеджер', 'KUZNEZOV.ANDREY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040873','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+
+-- cfo_id = E1040876
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040876','1678992', 56000001,'Управляющий магазином', 'KOLCZOV.MICHAIL', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040876','1661372', 56000002,'Территориальный менеджер', 'BELOZEROV.LEV', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040876','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+ 
+-- cfo_id = E1046351
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046351','1760149', 56000001,'Управляющий магазином', 'KATAFEEV.ALEXANDR', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046351','1639616', 56000002,'Территориальный менеджер', 'KUZNEZOV.ANDREY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046351','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+
+-- cfo_id = E1046341
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046341','1754784', 56000001,'Управляющий магазином', 'HAMETOV.RAIS', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046341','1661372', 56000002,'Территориальный менеджер', 'BELOZEROV.LEV', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1046341','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+
+-- cfo_id = E1047001
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047001','1763861', 56000001,'Управляющий магазином', 'ERMILOV.ALEXEY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047001','1652849', 56000002,'Территориальный менеджер', 'BABKOVA.IRINA', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047001','1600079', 56000003,'Директор по продажам', 'OSTANIN.DENIS', now());
+
+-- cfo_id = E1047053
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047053','1770770', 56000001,'Управляющий магазином', 'TUTMIN.SERGEY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047053','1639616', 56000002,'Территориальный менеджер', 'KUZNEZOV.ANDREY', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047053','1604646', 56000003,'Директор по продажам', 'ASTAHOVA.ULIYA', now());
+
+-- cfo_id = E1040875
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040875','1652849', 56000002,'Территориальный менеджер', 'BABKOVA.IRINA', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040875','1600079', 56000003,'Директор по продажам', 'OSTANIN.DENIS', now());
+
+-- cfo_id = E1045595
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1040875','1661476', 56000002,'Территориальный менеджер', 'FOROSTOVSKIY.RUSLAN', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1045595','1600079', 56000003,'Директор по продажам', 'OSTANIN.DENIS', now());
+
+-- cfo_id = E1047054
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047054','1652849', 56000002,'Территориальный менеджер', 'BABKOVA.IRINA', now());
+insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date) values ('E1047054','1600079', 56000003,'Директор по продажам', 'OSTANIN.DENIS', now());
