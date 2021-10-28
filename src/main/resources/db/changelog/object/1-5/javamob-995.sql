b512dcf43380cb7635f14dc0ff0aaf8c1852a916
Artem.Lambert <Artem.Lambert@x5.ru>
2021-10-28 12:15:35 +0300

    JAVAMOB-955 * Обновление привязок к магазинам "Чижик"

---
+++ src/main/resources/db/changelog/object/1-5/javamob-995.sql
@@ -0,0 +1,6 @@
+-- ДАННЫЙ СКРИП - НАПОМИНАНИЕ О ТОМ, ЧТО В БАЗУ ВРУЧНУЮ БЫЛА ВВЕДЕН НОВАЯ ПРИВЯЗКА ПО МАГАЗИНУ HD33 ЧИЖИК
+-- ТАК КАК, РАДИ ОДНОЙ ПРИВЯЗКИ, НЕ ХОТЕЛОСЬ ДЕЛАТЬ DT
+-- БЫЛ ДОБАВЛЕН "УМ" А.CHEGOLYA
+
+-- insert into bindings.employee(cfo_id, personal_number, position_id, position_name, personal_login, modified_date)
+-- values ('E1047835','1841688', 56000001,'Управляющий магазином', 'A.CHEGOLYA', now());
